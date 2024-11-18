51724ef6cfebe2650625eff76cc1922ac2bfcc70
Martinecko30 <52097833+Martinecko30@users.noreply.github.com>
2024-11-18 15:45:52 +0100

    Small fix to main loop

--- zlatybludistak.py
+++ zlatybludistak.py
@@ -1,3 +1,5 @@
+import traceback
+
 import pygame
 import sys, random, time, math
 import logger
@@ -11,39 +13,43 @@ MAX_FPS = 120
 def __main__():
     logger.start()
 
-    graphics.start()
-    running = True
+    try:
+        graphics.start()
+        running = True
 
-    player = c.Player(difficulty = "hard")
-    maze = mz.GameBoard(player.map_size[0])
-    maze.generate_board()
-    maze = mz.maze_generation_main()
+        player = c.Player(difficulty = "hard")
+        maze = mz.GameBoard(player.map_size[0])
+        maze.generate_board()
+        maze = mz.maze_generation_main()
 
-    clock = pygame.time.Clock()
+        clock = pygame.time.Clock()
 
-    last_time = 0
-    while running:
-        current_time = time.time()
-        delta_time = (current_time - last_time) / 1000
-        last_time = current_time
+        last_time = 0
+        while running:
+            current_time = time.time()
+            delta_time = (current_time - last_time) / 1000
+            last_time = current_time
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                terminate()
-            c.handle_player_movement(event, player, maze)
-        if c.check_if_is_over(player):
-            # Zavolat funkci endScreen
-            pass
-        print(player.pos_x, player.pos_y)
-        c.check_and_change_map(player)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    terminate()
+                c.handle_player_movement(event, player, maze)
+            if c.check_if_is_over(player):
+                # Zavolat funkci endScreen
+                pass
+            print(player.pos_x, player.pos_y)
+            c.check_and_change_map(player)
 
-        graphics.main_draw()
+            graphics.main_draw()
 
-        graphics.draw_fog_of_war(5)
+            graphics.draw_fog_of_war(5)
 
-        graphics.flip_display()
-        clock.tick(MAX_FPS)
+            graphics.flip_display()
+            clock.tick(MAX_FPS)
+    except:
+        logger.log(LogLevel.ERROR, traceback.format_exc())
+        terminate()
 
 def terminate():
     logger.end()
