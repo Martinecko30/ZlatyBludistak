ad23002ed13fb305d8f16553d7434eaf974d0fce
VonMeowstein <fulcrumstcz@gmail.com>
2024-11-25 18:03:03 +0100

    added end_screen

--- Graphics/Scenes.py
+++ Graphics/Scenes.py
@@ -1,1 +1,20 @@
 import pygame
+from pygame import Color
+from graphics import *
+from zlatybludistak import terminate
+
+def end_screen():
+
+    DISPLAY.fill('black')
+    message = "You won"
+    text_message = "You won"
+
+
+    text_surf = FONT.render('YOU WON', True, 'white')
+    text_rect = text_surf.get_rect()
+    text_rect.center = (WIDTH // 2 , HEIGHT // 2 - 30)
+
+    DISPLAY.blit(text_surf, text_rect)
+
+    pygame.display.update()
+
